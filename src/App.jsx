c28c540b7c18b8185fb2bec712a08f34a94976ad
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2022-09-24 20:21:52 +0100

    create app interface

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <h1>Hello World!!</h1>;
+  return (
+    <main className="main">
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container"></div>
+      <span className="roll-counter">Total Roll: 0</span>
+      <button className="roll-dice">Roll</button>
+    </main>
+  );
 }
 
 export default App;
