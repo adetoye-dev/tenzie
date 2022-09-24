ace2ed026dc247b4e8adcf1b1f206d39d0ba9fc3
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2022-09-24 21:52:32 +0100

    style die components to show icons

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react";
+
 const Die = (props) => {
-  const style = { backgroundColor: props.isHeld ? "#59E391" : "white" };
+  const style = { backgroundColor: props.isHeld ? "#59E391" : "grey" };
+  const [diceNumber, setDiceNumber] = useState("");
+
+  useEffect(() => {
+    if (props.value === 1) {
+      setDiceNumber("one");
+    } else if (props.value === 2) {
+      setDiceNumber("two");
+    } else if (props.value === 3) {
+      setDiceNumber("three");
+    } else if (props.value === 4) {
+      setDiceNumber("four");
+    } else if (props.value === 5) {
+      setDiceNumber("five");
+    } else if (props.value === 6) {
+      setDiceNumber("six");
+    }
+  }, []);
+
   return (
     <div className="die-face" style={style} onClick={props.holdDice}>
-      {props.value}
+      <h2 className="die-num">
+        <i className={`fa-solid fa-dice-${diceNumber}`}></i>
+      </h2>
     </div>
   );
 };
